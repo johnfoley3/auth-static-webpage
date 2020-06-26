4976f6b100a8d3898cb06dad937d06e29ae4c16b
John Foley <johnfoley3@gmail.com>
2020-06-25 20:57:41 -0500

    Update e2e specs for rendering files
    The E2E spec exercises the whole stack with injecting the route and
    resolving index.html files as necessary. This provides a solid
    iteration point to accurately testing changes moving forward without
    breaking wildcard resolution with file paths

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { readFileSync } from 'fs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -19,6 +20,27 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect(readFileSync('./html/index.html', 'utf-8'));
+  });
+
+  it('/dogs (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/dogs/')
+      .expect(200)
+      .expect(readFileSync('./html/dogs/index.html', 'utf-8'));
+  });
+
+  it('/cats (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/cats/')
+      .expect(200)
+      .expect(readFileSync('./html/cats/index.html', 'utf-8'));
+  });
+
+  it('404 Page (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/does-not-exist')
+      .expect(200)
+      .expect(readFileSync('./html/404.html', 'utf-8'));
   });
 });
